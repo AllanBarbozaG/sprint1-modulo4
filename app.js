7059d686d883c7d482f18cfa246e9ee05ea7fe06
allanbarbozag <allpatrick.b@gmail.com>
2022-07-14 10:37:24 -0300

    Adiciona o mini-projeto finalizado ao repositório remoto

---
+++ app.js
@@ -0,0 +1,23 @@
+const promptSync = require('prompt-sync')({sigint: true});
+const lista = [];
+
+function adicionaItens() {  
+  let entradaUsuario = promptSync('Digite um item:');
+  
+  while(true) {      
+    if (entradaUsuario.toUpperCase() == 'SAIR' || entradaUsuario.toUpperCase() == '/S') {
+      break;      
+    } else {
+      lista.push(entradaUsuario);
+      entradaUsuario = promptSync('Digite um item:');            
+    };
+  };  
+};
+
+function exibeLista() {
+  lista.sort().map((item) => console.log(item)); 
+};
+
+adicionaItens();
+exibeLista();
+
